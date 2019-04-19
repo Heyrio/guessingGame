8e0eb45dc6e5e72a96cc3539e3ca5e219ed44bea
Hamza Amer <hamer1563@gmail.com>
2019-04-19 01:27:16 -0400

    Add files via upload

--- public/js/indexJs.js
+++ public/js/indexJs.js
@@ -1,22 +1,25 @@
 
+window.onload = function (){
+
+// blip soind on h1 hover
+    $(document).ready(function(){
+        $("h1").hover(function(){
+            nav_audio.play();
+            },
+        function(){
+            nav_audio.load();
+        });
+    });
+    
 
 // Plays a wav file in a loop
-
-$(document).ready(function(){
-    $("h1").hover(function(){
-        nav_audio.play();
-        },
-    function(){
-        nav_audio.load();
-    });
-});
-
-
-
-
-myAudio = new Audio('./music/intro.wav'); 
-myAudio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, false);
-myAudio.play();
+    myAudio = new Audio('./music/intro.wav'); 
+    myAudio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    var promise = myAudio.play();
+    if(promise){
+        promise.catch(function(error) { console.log(error); });
+    }
+}
