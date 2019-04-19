686e1afb7b1b576b775a2c82fac24f46df72eda7
Hamza Amer <hamer1563@gmail.com>
2019-04-18 22:53:44 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
+//Database modules
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+// Name of the collection
+const collection = 'Winners'
+
+// mongoAtlas key
+const URL = 'mongodb+srv://heyrio:password@cluster0-7cpbz.mongodb.net/test?retryWrites=true';
+
+
+//middleware to parse the data
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 
 //directory for static files
 app.use(express.static(path.join(__dirname, '/public')))
@@ -23,17 +39,59 @@ app.get('/home', (req, res)=>{
     res.render('home');
 })
 
-app.get('/score',(req, res)=>{
-
-    res.render('score');
-
-});
 
 app.get('/winner',(req, res)=>{
 
     res.render('winner');
 
 });
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////// DATA BASE STUFF ///////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Connects to mongo Atlas
+// contains all post requests
+MongoClient.connect(URL,{useNewUrlParser: true},(error, client)=>{
+
+    if(error){
+        return console.log('Something went wrong couldnt connect to the database...')
+    }
+    const db = client.db(collection);
+
+    // Lets you store youre name in the winner page
+    app.post('/winner',(req, res)=>{
+        var userName = req.body.name
+        db.collection('User').insertOne({ name: userName });
+        res.render('winner')
+    })
+
+
+    // querys the database of all game winners
+    app.get('/score',(req, res)=>{
+        db.collection('User').find({}).toArray((error, data)=>{
+            if(error){
+                throw Error
+            }
+            var winners = [];
+            for(var i = 0; i <data.length; i++){
+                winners.push(data[i].name)
+            }
+            res.render('score', {
+                winners: winners + '\n'
+            });
+
+            
+        })     
+    
+    });
+    
+   
+    console.log('Connected to database..');
+})
 
 
 app.listen(3000,()=>{
